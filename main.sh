70c791df8c1f621ba07ac946feb6a23e1f69186d
Zhijun Zhao <zhijzhao@microsoft.com>
2017-01-17 16:58:55 +0800

    add main.sh

---
+++ main.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+save_device=0
+save_workingdir=0
+
+for arg in "$@"
+do   
+    if [ $save_device == 1 ]
+    then
+        save_device=0
+        if [ $arg == "edison" ]
+        then
+          imagename="edison"
+        elif [ $arg == "raspberrypi" ]
+        then
+          imagename="yungez/docker-raspberrypi"
+        else
+          echo unknow device name
+        fi
+    elif [ $save_workingdir == 1 ]
+    then
+        workingdir="$arg"
+        save_workingdir=0
+    else
+        case "$arg" in
+            "--device" ) save_device=1;;
+            "--workingdir" ) save_workingdir=1;;
+        esac
+    fi
+done
+
+echo $imagename
+echo $workingdir
+
+#docker pull yungez/docker-edison
+docker run -i -v $workingdir:/source $imagename build
