2239e110b5c6b3949314ea608466c11fc0bfe282
ZhijunZhao <zhijun.zhao@outlook.com>
2017-02-09 13:28:38 +0800

    add build dir support (#3)
    * improve build
    
    * support build dir
    
    * cleanup

--- dockerfiles/build_edison.sh
+++ dockerfiles/build_edison.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+save_builddir=0
+
+for arg in "$@"
+do   
+    if [ $save_builddir == 1 ]
+    then
+        builddir="/source/$arg"
+        save_builddir=0
+    else
+        case "$arg" in
+            "--builddir" ) save_builddir=1;;
+        esac
+    fi
+done
+
+if [ -z ${builddir+x} ]; then 
+    builddir=/source
+fi
+
+if [ ! -d "$builddir" ]; then
+    echo no build dir $builddir
+    exit 1
+fi
+
 if [ ! -d "/source" ]; then
     echo no source dir /source
     exit 1
@@ -17,8 +41,10 @@ if [ -f "/source/build.log" ]; then
     rm "/source/build.log"
 fi
 
-cd /source && cmake -DCMAKE_TOOLCHAIN_FILE=/home/toolchain-edison.cmake -Dazure_IoT_Sdk_c=/azure-iot-sdk-c  .
+pushd $builddir > /dev/null
+cmake -DCMAKE_TOOLCHAIN_FILE=/home/toolchain-edison.cmake -Dazure_IoT_Sdk_c=/azure-iot-sdk-c  /source
 make
+popd > /dev/null
 
 if [ $? -eq 0 ]; then
     echo Build succeeded!
