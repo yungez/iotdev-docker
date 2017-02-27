4cc875d33be8f0abcdaa4e2c52ac3f39bba0ee6d
yungez <yungez@microsoft.com>
2017-02-27 10:31:19 +0800

    add bi support

--- dockerfiles/index.sh
+++ dockerfiles/index.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+task=""
 
 if [ "$1" == "build" ]
 then
   source /build.sh
+  task=build
 elif [ "$1" == "deploy" ]
 then
   source /deploy.sh
+  task=deploy
 else
   echo unknown command
 fi
+
+device=unknowndevice
+#source /bi.sh --device $device --event $task
